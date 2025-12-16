5a46c91aa5a03c541560319d69645fbee2ed603d
priti-fonix <priti.kumari@Fonixtech.io>
2025-12-16 12:10:04 +0530

    Add chai stores and menu API endpoints with JSON responses

--- chai_django/chai/urls.py
+++ chai_django/chai/urls.py
@@ -5,7 +5,16 @@ from . import views
 
 urlpatterns = [
     
-    path('history/', views.chai_details, name='chai_details'),
-    path('menu/', views.chail_menu, name='chail_menu')
+    path('store/', views.chai_stores, name='chai_stores'),
+    path('store/<int:chai_id>/', views.chai_storesById, name='chai_store_by_id'),
+    path('menu/', views.chai_menu, name='chai_menu')
 
 ]
+
+'''
+from django.contrib.auth.models import User
+user = User.objects.get(username='admin')
+user.set_password('new_password')
+user.save()
+
+'''
--- chai_django/chai/views.py
+++ chai_django/chai/views.py
@@ -1,15 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 # from django.conf import 
-from . import models
+from .models import chai_stores as ChaiStores, chai_menu as ChaiMenu
 
 # Create your views here.
 
-def chai_details(request):
-    return render(request,'chai.html')
+# def chai_details(request):
+#     chai = models.chai_menu.objects.all()
+#     return HttpResponse({'chai':chai})
 
-def chail_menu(request):
-    return HttpResponse(models.chai_menu)
+def chai_menu(request):
+    chais = ChaiMenu.objects.all()
+    chai_list = list(chais.values())
+    return JsonResponse({'chai': chai_list}, safe=False)
 
-def chail_menu(request):
-    return HttpResponse(models.chai_stores)
+def chai_stores(request):
+    stores = ChaiStores.objects.all()
+    store_list = list(stores.values())
+    return JsonResponse({'chai_stores': store_list}, safe=False)
+
+def chai_storesById(request, chai_id):
+    # store = ChaiStores.objects.get(pk=chai_id)
+    # store_data = {
+    #     'id': store.id,
+    #     'name': store.name,
+    #     'description': store.description,
+    #     'distance': store.distance,
+    #     'type': store.type
+    # }
+    store = get_object_or_404(ChaiStores, pk=chai_id)
+    store_data = {
+        'id': store.id,
+        'name': store.name,
+        'description': store.description,
+        'distance': store.distance,
+        'type': store.type
+    }
+
+    return JsonResponse({'chai_store': store_data}, safe=False)
